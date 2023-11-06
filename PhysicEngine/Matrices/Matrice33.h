ab296f25dbba1105782c9b27ebe9203dead025c9
erwan <erwan.chevalier@isen-ouest.yncrea.fr>
2023-11-06 16:36:06 +0100

    fix matrice33

--- PhysicEngine/Matrices/Matrice33.h
+++ PhysicEngine/Matrices/Matrice33.h
@@ -8,6 +8,10 @@ class Matrice33
 {
 public:
 
+	Matrice33();
+
+	Matrice33(float newVal[9]);
+
 	//combinaison de transformation lineaire
 	Matrice33 operator*(const Matrice33& m) const;
 
@@ -25,15 +29,18 @@ public:
 	// set the matrix base on a quaternion
 	void setOrientation(const Quaternion& q);
 
-	float& getValues(int i) { return value[i]; }
+	float getDeterminant(float* tab, int n);
+
+	float& getValues(int i) { return values[i]; }
 
 
 
 private:
 
-	float value[9];
+	float values[9];
 	float getDeterminant();
 	Matrice33 getAdjointe() const;
+	void getCofactor(Matrice33& vec, Matrice33& temp, int p, int q) const;
 };
 
 
